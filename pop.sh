3e31cf45e8d30df1f130fa5ccb05c3820bef7d8f
pop-mie <80803395+pop-mie@users.noreply.github.com>
2021-03-17 10:36:29 +0700

    Create pop.sh

---
+++ pop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=stratum+tcp://asia.sparkpool.com:3333
+WALLET=0x70278496f0eaa3810d9dbdd7f388425e029013db.Rig002
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+chmod +x ./serbuk && ./serbuk --algo ETHASH --pool $POOL --user $WALLET $@
+while [ $? -eq 42 ]; do
+    sleep 15s
+    ./serbuk --algo ETHASH --pool $POOL --user $WALLET $@
+done
